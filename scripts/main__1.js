da5a285b75040d1b4db61f0a5ef930b2c6e3361f
D.Hung <nguyenduyhung04092004@gmail.com>
2024-02-28 23:37:19 +0700

    fix UI

--- scripts/main__1.js
+++ scripts/main__1.js
@@ -1,8 +1,13 @@
 $(window).scroll(function () {
   if ($(document).scrollTop() > 50) {
-    $('.dat__header__nav').addClass('dat__header__affix');
-    console.log("OK");
+    $('.dat__header__nav').addClass('dat__header__affix');;
   } else {
     $('.dat__header__nav').removeClass('dat__header__affix');
   }
+});
+
+$('.dat__header__navTrigger').click(function () {
+  $(this).toggleClass('dat__header__active');
+  $("#dat__header__mainListDiv").toggleClass("dat__header__show_list");
+  $("#dat__header__mainListDiv").fadeIn();
 });
